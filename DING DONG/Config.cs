e471e8f145c1dbec1f0647bedf2694b401cddcb9
Shaahin Faezi <shaahinfaezi@gmail.com>
2020-07-09 21:54:47 +0430

    Revert "Revert "This is gonna work for everyone!""
    This reverts commit 8be5de4f39c362e7c44a0be992df758ffaf10ddb.

--- DING DONG/Config.cs
+++ DING DONG/Config.cs
@@ -36,6 +36,8 @@ namespace DING_DONG
             public string token;
             public string cmdPrefix;
             public string clientid;
+            public string channelid;
+            public string serverlid;
         }
     }
 }
